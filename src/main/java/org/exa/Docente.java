744de9efc6af38583a85b872e6fc24ba252a3524
Franco Arellano <francoare@live.com>
2022-10-06 16:15:38 -0300

    Se agrego la estructura general del proyecto

---
+++ src/main/java/org/exa/Docente.java
@@ -0,0 +1,9 @@
+package org.exa;
+
+public class Docente {
+    private String nombre;
+    private int horasT;
+    private int horasP;
+    private int horasTP;
+    private int horasPE;
+}
---
+++ src/main/java/org/exa/FileManager.java
@@ -0,0 +1,15 @@
+package org.exa;
+
+public class FileManager {
+    
+    public static void cargarCatedra(String directorio){}
+
+    public static void cargarDocente(String directorio){}
+
+    public static void guardarFormula(String formula){}
+
+    public static void cargarFormula(String directorio){}
+
+    public static void generarSalida(){}
+
+}
