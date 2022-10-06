409cbb480b2c6ef2b4a307250effff8262c5f83b
Franco Arellano <francoare@live.com>
2022-10-06 16:15:38 -0300

    Se agrego la estructura general del proyecto

---
+++ src/main/java/org/exa/Catedra.java
@@ -0,0 +1,16 @@
+package org.exa;
+
+import java.util.List;
+
+public class Catedra {
+    private String nombre;
+    private int horasT;
+    private int horasP;
+    private int horasTP;
+    private int horasPE;
+
+    private int cantInscriptos;
+    private int cantRindieron;
+
+    private List<Docente> docentes;
+}
---
+++ src/main/java/org/exa/Estructura.java
@@ -0,0 +1,11 @@
+package org.exa;
+
+import java.util.List;
+import java.util.Map;
+
+public class Estructura {
+    public static List<Catedra> catedras;
+    public static Map<String, Integer> resultado;
+    public static String formula; 
+    public static String pathFormula;
+}
