f420947f71dbc3b94a1f38a5b50f66105b134dcf
Franco Arellano <francoare@live.com>
2022-11-02 00:30:40 -0300

    commit inicial del segundo sprint

--- src/main/java/org/exa/Catedra.java
+++ src/main/java/org/exa/Catedra.java
@@ -5,19 +5,30 @@ import java.util.List;
 
 public class Catedra {
     private String nombre;
-    private int anioMateria;
+    private HashMap<String, Float> variables;
+    private List<Docente> docentes;
+
+    /*private int anioMateria;
     private int horasT;
     private int horasP;
     private int horasTP;
     private int horasPE;
 
-    /* 0 = sin lab |  1 = lab de informatica | 
-       2 = lab para practicas experimentales | 3 = practicas docentes
-    */
+       //0 = sin lab |  1 = lab de informatica | 
+       //2 = lab para practicas experimentales | 3 = practicas docentes
+    
     private int tipoPE; //AGREGAR A ARCHIVO. 
     private int cantInscriptos;
     private int cantRindieron;
-    private List<Docente> docentes;
+    */
+    
+    //---------------------------CONSTRUCTOR------------------------------------------- 
+
+    public Catedra(String nombre) {
+        this.nombre = nombre;
+        variables = new HashMap<>();
+        docentes = new ArrayList<>();
+    }
 
 
     //---------------------------SETTERS-----------------------------------------------
@@ -26,41 +37,41 @@ public class Catedra {
         this.nombre = nombre;
     }
 
-    public void setAnioMateria(int anioMateria) {
-        this.anioMateria = anioMateria;
-    }
+    // public void setAnioMateria(int anioMateria) {
+    //     this.anioMateria = anioMateria;
+    // }
 
-    public void setHorasT(int horasT) {
-        this.horasT = horasT;
-    }
+    // public void setHorasT(int horasT) {
+    //     this.horasT = horasT;
+    // }
 
-    public void setHorasP(int horasP) {
-        this.horasP = horasP;
-    }
+    // public void setHorasP(int horasP) {
+    //     this.horasP = horasP;
+    // }
 
-    public void setHorasTP(int horasTP) {
-        this.horasTP = horasTP;
-    }
+    // public void setHorasTP(int horasTP) {
+    //     this.horasTP = horasTP;
+    // }
 
-    public void setHorasPE(int horasPE) {
-        this.horasPE = horasPE;
-    }
+    // public void setHorasPE(int horasPE) {
+    //     this.horasPE = horasPE;
+    // }
 
-    public void setTipoPE(char tipoPE) {
-        this.tipoPE = tipoPE;
-    }
+    // public void setTipoPE(char tipoPE) {
+    //     this.tipoPE = tipoPE;
+    // }
 
-    public void setCantInscriptos(int cantInscriptos) {
-        this.cantInscriptos = cantInscriptos;
-    }
+    // public void setCantInscriptos(int cantInscriptos) {
+    //     this.cantInscriptos = cantInscriptos;
+    // }
 
-    public void setCantRindieron(int cantInscriptos) {
-        this.cantInscriptos = cantInscriptos;
-    }
+    // public void setCantRindieron(int cantInscriptos) {
+    //     this.cantInscriptos = cantInscriptos;
+    // }
 
-    public void setDocentes (List<Docente> docentes) {
-        this.docentes = docentes;
-    }
+    // public void setDocentes (List<Docente> docentes) {
+    //     this.docentes = docentes;
+    // }
 
      //-----------------------------GETTERS-----------------------------------------
 
@@ -68,62 +79,47 @@ public class Catedra {
         return nombre;
     }
 
-    public int getAnioMateria() {
-        return anioMateria;
-    }
+    // public int getAnioMateria() {
+    //     return anioMateria;
+    // }
 
-    public int getHorasT() {
-        return horasT;
-    }
+    // public int getHorasT() {
+    //     return horasT;
+    // }
 
-    public int getHorasP() {
-        return horasP;
-    }
+    // public int getHorasP() {
+    //     return horasP;
+    // }
 
-    public int getHorasTP() {
-        return horasTP;
-    }
+    // public int getHorasTP() {
+    //     return horasTP;
+    // }
 
-    public int getHorasPE() {
-        return horasPE;
-    }
+    // public int getHorasPE() {
+    //     return horasPE;
+    // }
 
-    public int getTipoPE(){
-        return tipoPE;
-    }
+    // public int getTipoPE(){
+    //     return tipoPE;
+    // }
 
-    public int getCantInscriptos() {
-        return cantInscriptos;
-    }
+    // public int getCantInscriptos() {
+    //     return cantInscriptos;
+    // }
 
-    public int getCantRindieron() {
-        return cantRindieron;
-    }
+    // public int getCantRindieron() {
+    //     return cantRindieron;
+    // }
 
-    public List<Docente> getDocentes() {
-        List<Docente> aux = this.docentes;
-        return aux;
-    }
+    // public List<Docente> getDocentes() {
+    //     List<Docente> aux = this.docentes;
+    //     return aux;
+    // }
 
-    public int getCantDocentes() {
-        return docentes.size();
-    }
-    
+    // public int getCantDocentes() {
+    //     return docentes.size();
+    // }
 
-
-    public Catedra(String nombre, int anioMateria, int horasT, int horasP, int horasTP, int horasPE, int tipoPE,
-            int cantInscriptos, int cantRindieron) {
-        this.nombre = nombre;
-        this.anioMateria = anioMateria;
-        this.horasT = horasT;
-        this.horasP = horasP;
-        this.horasTP = horasTP;
-        this.horasPE = horasPE;
-        this.tipoPE = tipoPE;
-        this.cantInscriptos = cantInscriptos;
-        this.cantRindieron = cantRindieron;
-        docentes = new ArrayList<>();
-    }
 
 
     public void agregarDocente(Docente docente){
